f1b4ec721af03c2e065d3f93eebebd0dc9b841f0
NMB <numbzinho159@gmail.com>
2024-11-02 21:23:45 -0300

    javascript para alterar os telefones quando clicar em outro botão

--- js/script.js
+++ js/script.js
@@ -1,8 +1,26 @@
 const dots = document.querySelectorAll(".dot");
+const phoneImage = document.querySelector(".phoneimg img");
 
-dots.forEach((dot) => {
-  dot.addEventListener("click", () => {
-    dots.forEach((d) => d.classList.remove("active"));
-    dot.classList.add("active");
-  });
+dots.forEach((dot, index) => {
+    dot.addEventListener("click", () => {
+        dots.forEach((d) => d.classList.remove("active"));
+        dot.classList.add("active");
+        phoneImage.src = Phones[index].imgSrc;
+    });
 });
+
+const Phones = [
+  {
+    imgSrc: "./images/phone1.webp",
+  },
+  {
+    imgSrc: "./images/phone2.webp",
+  },
+  {
+    imgSrc: "./images/phone3.webp",
+  },
+  {
+    imgSrc: "./images/phone4.webp",
+  },
+];
+
